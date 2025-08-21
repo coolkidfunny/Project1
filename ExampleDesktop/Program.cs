2f5235f6a074642322c91050fb88f010305ed3fc
Rafferti Smith <88866816+coolkidfunny@users.noreply.github.com>
2025-08-21 18:39:34 +1000

    Add project files.

---
+++ ExampleDesktop/Program.cs
@@ -0,0 +1,19 @@
+namespace ExampleDesktop
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            PersonDLL.Person randomGuy = new PersonDLL.Person().randomStats();
+            randomGuy.printStats();
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
